77285e0c8b168395190e73040c6b11b878b1da6a
Angus H. <4148@users.noreply.github.com>
2015-01-06 04:23:17 -0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -8,13 +8,18 @@ using namespace std;
 #include "windows.h"
 #include "Psapi.h"
 
-// helper function prototypes
+// util function prototypes
 string basename(string path);
 wstring basename(wstring path);
 wstring to_wstring(string s);
 wstring to_upper(wstring ws);
-HMODULE get_base_address(HANDLE process_handle, wstring path);
 void print_format(string s, unsigned int width, string delim);
 int to_int(string s);
 HMODULE to_hex(string s);
 DWORD64 to_hex(char c);
+
+// WINAPI utilities
+bool check_winapi_error(DWORD expected, DWORD result, string function_name, bool error_exit);
+enum PROCESS_MODE { USER_MODE, KERNEL_MODE };
+void remove_permissions(HANDLE process_handle, PROCESS_MODE process_mode);
+HMODULE get_base_address(HANDLE process_handle, wstring path);
